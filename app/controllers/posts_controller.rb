ddb08a3ab8e07eddc6c596be54c19152ce9be7f3
Wahid Rahim <wahidrahim@gmail.com>
2015-04-06 05:38:21 -0400

    basic pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def index
-    @posts = reddit.front_page(options)
+    @posts = reddit.front_page(options).results
+    @first_id = @posts.first.id
+    @last_id = @posts.last.id
   end
 
   def subreddit
@@ -13,6 +15,7 @@ class PostsController < ApplicationController
   def options
     {category: params[:category],
      time: params[:time],
-     after: params[:after]}
+     after: params[:after],
+     before: params[:before]}
   end
 end
